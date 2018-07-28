9db5e324c9857c84fd0e76e3244bb69c7f1b45e2
Xab3r <mail.xaber@gmail.com>
2018-07-28 14:58:16 +0300

    Minor refactoring

--- Source/TailBlazer.Domain/Infrastructure/EnumDescriptionTypeConverter.cs
+++ Source/TailBlazer.Domain/Infrastructure/EnumDescriptionTypeConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 
 namespace TailBlazer.Domain.Infrastructure
 {
@@ -13,25 +14,26 @@ namespace TailBlazer.Domain.Infrastructure
         public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture,
             object value, Type destinationType)
         {
-            if (destinationType == typeof(string))
+            if (destinationType != typeof(string))
             {
-                if (value != null)
-                {
-                    var fi = value.GetType().GetField(value.ToString());
-                    if (fi != null)
-                    {
-                        var attributes =
-                            (DescriptionAttribute[]) fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
-                        return ((attributes.Length > 0) && (!String.IsNullOrEmpty(attributes[0].Description)))
-                            ? attributes[0].Description
-                            : value.ToString();
-                    }
-                }
+                return base.ConvertTo(context, culture, value, destinationType);
+            }
 
+            if (value == null)
+            {
                 return string.Empty;
             }
 
-            return base.ConvertTo(context, culture, value, destinationType);
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            if (fieldInfo == null)
+            {
+                return string.Empty;
+            }
+
+            var attributes = (DescriptionAttribute[]) fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.FirstOrDefault(x => !string.IsNullOrEmpty(x.Description))?.Description ??
+                   value.ToString();
         }
     }
 }
--- Source/TailBlazer.Domain/Infrastructure/EnumToItemsSource.cs
+++ Source/TailBlazer.Domain/Infrastructure/EnumToItemsSource.cs
@@ -5,48 +5,29 @@ namespace TailBlazer.Domain.Infrastructure
 {
     public class EnumBindingSourceExtension : MarkupExtension
     {
-        private Type _enumType;
-
-        public Type EnumType
-        {
-            get { return this._enumType; }
-            set
-            {
-                if (value != this._enumType)
-                {
-                    if (null != value)
-                    {
-                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
-                        if (!enumType.IsEnum)
-                            throw new ArgumentException("Type must be for an Enum.");
-                    }
-
-                    this._enumType = value;
-                }
-            }
-        }
-
-        public EnumBindingSourceExtension()
-        {
-        }
+        private readonly Type _enumType;
 
         public EnumBindingSourceExtension(Type enumType)
         {
-            this.EnumType = enumType;
+            _enumType = enumType;
         }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             if (null == this._enumType)
+            {
                 throw new InvalidOperationException("The EnumType must be specified.");
+            }
 
-            Type actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
-            Array enumValues = Enum.GetValues(actualEnumType);
+            var actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
+            var enumValues = Enum.GetValues(actualEnumType);
 
             if (actualEnumType == this._enumType)
+            {
                 return enumValues;
+            }
 
-            Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+            var tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
             enumValues.CopyTo(tempArray, 1);
             return tempArray;
         }
